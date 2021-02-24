f341364b7fdfee70b8ecbff40dcdc5bd6f0c51c5
John Doe <johndoe@example.com>
2021-02-24 12:36:03 +0600

    Lab Task-7 Footer Update

---
+++ MID_LAB_TASK_PHP_SESSION_07/Footer.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        .footer
+        {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            text-align: center;
+        }
+    </style>
+</head>
+    <bodY>
+        
+        <footer>
+            
+            <div class="footer">
+                <fieldset>
+                <h5>Copyright @ 2017</h5>
+            </fieldset>
+            </div>
+        
+        </footer>
+    
+    </bodY>
+</html>
