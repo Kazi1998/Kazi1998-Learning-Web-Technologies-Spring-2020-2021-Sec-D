e4630a9a2b64c82e79924473ec31415c45bcfeac
John Doe <johndoe@example.com>
2021-02-28 20:46:57 +0600

    Lab Task-7 Home-1

---
+++ MID_LAB_TASK_PHP_SESSION_07/Home1.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        body
+        {
+            background: white;
+            width: 100%;
+            text-align: justify;
+        }
+        div.about
+        {
+            width: 710px;
+            display: inline-block;
+            text-align: right;
+            margin: 20px;
+            
+        }
+        div.information
+        {
+            font-family: Calibri;
+            width: 1250px;
+            text-align: right;
+        }
+    </style>
+    <body>
+    <?php include('Header1.php');?>
+        <h1 style="font-size:50px;text-align: center;">
+        </h1>
+        <hr class="line">
+        <div class="about">
+            <p style="font-size: 20px; translate: 7%;">Welcome to Bob</p>
+        <tr>
+            <td colspan="2">
+<table  align="left"  >
+    <tr>
+        
+            <td width="100%">
+                <label><b>Account</b></label>
+<hr>
+                <table> 
+                <tr>
+                    <td>
+                <ul>
+                    <ol>
+    <li><a href="#">Dashboard</a></li>
+    <li><a href="#">View Profile</a></li>
+    <li><a href="#">Edit Profile</a></li>
+    <li><a href="#">Change Profile picture</a></li>
+    <li><a href="#">Change Password</a></li>
+    <li><a href="#">Logout</a></li>
+</ol>
+</ul>
+</td>
+</table>
+        </div>
+        
+    <?php include('Footer.php');?>
+    </body>
+</html>
